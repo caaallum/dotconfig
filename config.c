9f29e34614c8fb280f7a2336e35ca0cb6fbcc355
caaallum <callum_speirs@protonmail.com>
2023-03-12 22:11:08 +0000

    Update config

--- config.c
+++ config.c
@@ -23,23 +23,52 @@
 #include <sys/stat.h>
 #include <limits.h>
 #include <stdio.h>
+#include <pwd.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+
+char home[PATH_MAX];
 
 int
 config__dir_exists(const char *path) {
     struct stat sb;
-    return stat(path, &sb) == 0 && S_ISDIR(sb.st_mode);
+    return stat(path, &sb) != 0 && S_ISDIR(sb.st_mode);
 }
 
 int
 config_install(const char *path) {
     char config[PATH_MAX];
+    struct passwd *pw = getpwuid(getuid());
+    strcpy(home, pw->pw_dir);
 
-    sprintf(config, "%s/.config", path);
+    sprintf(config, "%s/config", path);
+    
+    if (!config__dir_exists(config)) {
+        fprintf(stderr, "config not found in %s\n", path);
+        return 0;
+    }
 
     if (config__dir_exists(config)) {
         printf(".config exists\n");
     } else {
         printf(".config doesnt exist\n");
+        mkdir(path, 0600);
+    }
+
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(path);
+    if (d) {
+        while((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
+            char from[PATH_MAX], to[PATH_MAX];
+            sprintf(from, "%s/%s", path, dir->d_name);
+            sprintf(to, "%s/.config/%s", home, dir->d_name);
+            printf("Creating link from: %s to: %s\n", from, to);
+            //symlink(from, to);
+        }
+        closedir(d); 
     }
 
     return 0;
